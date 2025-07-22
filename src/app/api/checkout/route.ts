bcf5e97734733f22a4e0e4e24f3f6ceaa104dc08
Aman-web-dev <amanzhx1234+github@gmail.com>
2025-07-22 14:53:52 +0530

    Added Stripe initialization in The Post Function Body

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,17 +1,16 @@
 import Stripe from 'stripe';
 
 
-const STRIPE_API_KEY=process.env.STRIPE_API_KEY as string
-const YOUR_DOMAIN = process.env.HOSTED_DOMAIN;
-const PRICE_ID = process.env.PRICE_ID;
-
-
+const STRIPE_API_KEY=process.env.NEXT_PUBLIC_STRIPE_API_KEY as string
+const YOUR_DOMAIN = process.env.NEXT_PUBLIC_HOSTED_DOMAIN;
+const PRICE_ID = process.env.NEXT_PUBLIC_PRICE_ID;
 console.log(STRIPE_API_KEY,YOUR_DOMAIN,PRICE_ID);
+
+export async function POST(req:any,res:any){
 
 const stripe = new Stripe(STRIPE_API_KEY);
 
-export async function POST(req:any,res:any){
-      const session = await stripe.checkout.sessions.create({
+  const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         price: PRICE_ID,
